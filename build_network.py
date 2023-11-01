ea5c5d7f5c3cae571a06fdf88ba6773ecdd80e81
leonardozilli <leonardo.zilli19@gmail.com>
2023-11-01 11:27:50 +0100

    network.py

---
+++ build_network.py
@@ -0,0 +1,82 @@
+import json
+import pandas as pd
+import networkx as nx
+from pprint import pprint
+import matplotlib.pyplot as plt
+
+
+G = nx.Graph()
+
+with open('data/internalAuthors.json', 'r') as f:
+    authors = json.load(f)
+
+with open('data/publications.json', 'r') as f:
+    publications = json.load(f)
+
+def add_authors(graph):
+
+    for author in authors:
+        G.add_node(authors[author]['Nome completo'], id=author, affiliation=authors[author]['Afferenza'])
+
+def add_edges(graph):
+    
+    for prof in publications:
+        for link in publications[prof]:
+            for author in publications[prof][link]['internalAuthor']:
+                for coauthor in publications[prof][link]['internalAuthor']:
+                    if author != coauthor:
+                        G.add_edge(authors[author]['Nome completo'], authors[coauthor]['Nome completo'], publication=publications[prof][link]['dc.title'])
+
+
+def visualize_graph(graph):
+    color_palette = {
+        'DIPARTIMENTO DI FILOLOGIA CLASSICA E ITALIANISTICA': 'gold',
+        'DIPARTIMENTO DI INFORMATICA - SCIENZA E INGEGNERIA': 'green',
+    }
+
+    prof_list = ['PERONI, SILVIO', 'TOMASI, FRANCESCA', 'VITALI, FABIO', 'PESCARIN, SOFIA', 'GANGEMI, ALDO', 'ITALIA, PAOLA', 'TAMBURINI, FABIO']
+
+    plt.figure(figsize=(15,15))
+    nx.draw(graph, 
+            node_color=[color_palette[node[1]['affiliation']] if color_palette.get(node[1]['affiliation']) else 'blue' for node in G.nodes(data=True)], 
+            node_size = [800 if node[0] in prof_list else 10 for node in G.nodes(data=True)],
+            with_labels=True)
+    plt.show()
+
+def analysis(graph):
+    betweenness_centrality = nx.betweenness_centrality(G)
+    degree_centrality = nx.degree_centrality(G)
+    closeness_centrality = nx.closeness_centrality(G)
+
+    bc_data = pd.DataFrame.from_dict(betweenness_centrality, 
+                                columns=["BetweennessCentrality"], 
+                                orient="index")
+
+    dc_data = pd.DataFrame.from_dict(degree_centrality, 
+                                columns=["DegreeCentrality"], 
+                                orient="index")
+
+    cc_data = pd.DataFrame.from_dict(closeness_centrality, 
+                                columns=["ClosenessCentrality"], 
+                                orient="index")
+
+    centrality_data = pd.concat([bc_data, dc_data, cc_data], axis=1)
+
+    print(centrality_data.sort_values(by=['ClosenessCentrality'], ascending=False))
+
+def main():
+    add_authors(G)
+    add_edges(G)
+    print(G)
+    analysis(G)
+    visualize_graph(G)
+
+if __name__ == '__main__':
+    main()
+
+
+"""
+todo:
+    - add weights to edges
+    - associate departments to colors
+"""
