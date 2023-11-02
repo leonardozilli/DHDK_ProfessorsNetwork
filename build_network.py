fc608ec46f66cba32f79bd1b346a2194d7185189
leonardozilli <leonardo.zilli19@gmail.com>
2023-11-02 22:32:17 +0100

    added weights and publications to edges, removed duplicates

--- build_network.py
+++ build_network.py
@@ -4,6 +4,7 @@ import numpy as np
 import networkx as nx
 from pprint import pprint
 import matplotlib.pyplot as plt
+from itertools import combinations
 
 
 G = nx.Graph()
@@ -11,7 +12,7 @@ G = nx.Graph()
 with open('data/internalAuthors.json', 'r') as f:
     authors = json.load(f)
 
-with open('data/publications.json', 'r') as f:
+with open('data/unique_publications.json', 'r') as f:
     publications = json.load(f)
 
 def add_authors(graph):
@@ -19,12 +20,13 @@ def add_authors(graph):
         G.add_node(authors[author]['Nome completo'], id=author, affiliation=authors[author]['Afferenza'])
 
 def add_edges(graph):
-    for prof in publications:
-        for link in publications[prof]:
-            for author in publications[prof][link]['internalAuthor']:
-                for coauthor in publications[prof][link]['internalAuthor']:
-                    if author != coauthor:
-                        G.add_edge(authors[author]['Nome completo'], authors[coauthor]['Nome completo'], publication=publications[prof][link]['dc.title'])
+    for link in publications:
+        for author, coauthor in combinations(publications[link]['internalAuthor'], 2):
+            if G.has_edge(authors[author]['Nome completo'], authors[coauthor]['Nome completo']):
+                G[authors[author]['Nome completo']][authors[coauthor]['Nome completo']]['weight'] += 1
+                G[authors[author]['Nome completo']][authors[coauthor]['Nome completo']]['publications'].append(link)
+            else:
+                G.add_edge(authors[author]['Nome completo'], authors[coauthor]['Nome completo'], weight=1, publications=[link])
 
 
 def visualize_graph(graph):
@@ -49,13 +51,12 @@ def visualize_graph(graph):
     nx.draw(graph, 
             pos,
             node_color=[color_palette[node[1]['affiliation']] if color_palette.get(node[1]['affiliation']) else 'blue' for node in G.nodes(data=True)], 
-            node_size = [800 if node[0] in prof_list else 10 for node in G.nodes(data=True)],
-            with_labels=True)
+            node_size = [800 if node[0] in prof_list else 10 for node in G.nodes(data=True)],)
     for node, data in G.nodes(data=True):
         if node in prof_list:
             nx.draw_networkx_labels(G, pos, labels={node: node}, font_size=12, font_color='black', verticalalignment='top', bbox=bbox_props)
         else:
-            nx.draw_networkx_labels(G, pos, labels={node: node}, font_size=2, font_color='black', verticalalignment='top')
+            nx.draw_networkx_labels(G, pos, labels={node: node}, font_size=5, font_color='black', verticalalignment='top')
 
 
     plt.show()
@@ -64,6 +65,7 @@ def analysis(graph):
     betweenness_centrality = nx.betweenness_centrality(G)
     degree_centrality = nx.degree_centrality(G)
     closeness_centrality = nx.closeness_centrality(G)
+    eigenvector_centrality = nx.eigenvector_centrality(G)
 
     bc_data = pd.DataFrame.from_dict(betweenness_centrality, 
                                 columns=["BetweennessCentrality"], 
@@ -77,15 +79,20 @@ def analysis(graph):
                                 columns=["ClosenessCentrality"], 
                                 orient="index")
 
-    centrality_data = pd.concat([bc_data, dc_data, cc_data], axis=1)
+    ec_data = pd.DataFrame.from_dict(eigenvector_centrality, 
+                                columns=["EigenvectorCentrality"], 
+                                orient="index")
 
-    print(centrality_data.sort_values(by=['ClosenessCentrality'], ascending=False))
+    centrality_data = pd.concat([bc_data, dc_data, cc_data, ec_data], axis=1)
+
+    print(centrality_data.sort_values(by=['BetweennessCentrality'], ascending=False).head())
 
 def main():
     add_authors(G)
     add_edges(G)
-    print(G)
     analysis(G)
+    print(G)
+    #most recurring collaborations -> print(sorted(G.edges(data=True),key= lambda x: x[2]['weight'],reverse=True)[0])
     visualize_graph(G)
 
 if __name__ == '__main__':
@@ -95,5 +102,6 @@ if __name__ == '__main__':
 """
 todo:
     - add weights to edges
+    - add keywords
     - associate departments to colors
 """
